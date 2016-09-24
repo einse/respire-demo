5511dc84ba1465862168ef704f68ef939a17544d
Serguey Arsentev <einse@yandex.ru>
2016-09-24 20:40:21 +0300

    Redundant 'enter' key handler was deleted.

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-$(document).keypress(function (e) {
-	if (e.keyCode === 13) { /* enter */
-		// Create new entry
-		var focused = $(".entry:focus");
-		focused.after('<div class="entry" contentEditable="true"></div>').next().focus();
-		e.preventDefault();
-	}
-});
-
 $(".entry:first").html(Calendar.today('ru'));
 $(".entry:last").focus();
