2a1ee56568ba2de941c52e929a0fb9b2d7d2eaa3
Serguey Arsentev <einse@yandex.ru>
2016-09-26 20:43:37 +0300

    Add an array for month names (ru); add reversed numeric representation

--- calendar.js
+++ calendar.js
@@ -1,50 +1,31 @@
+/**
+ * Calendar is a part of Respire:
+ * https://github.com/einse/respire
+ */
+
 var Calendar = {
-	today: function(locale) {
+	today: function (param) {
 		var d = new Date();
-		var month = d.getMonth() + 1;
-		if (locale === 'ru') {
-			switch (month) {
-				case 1:
-					month = "января";
-					break;
-				case 2:
-					month = "февраля";
-					break;
-				case 3:
-					month = "марта";
-					break;
-				case 4:
-					month = "апреля";
-					break;
-				case 5:
-					month = "мая";
-					break;
-				case 6:
-					month = "июня";
-					break;
-				case 7:
-					month = "июля";
-					break;
-				case 8:
-					month = "августа";
-					break;
-				case 9:
-					month = "сентября";
-					break;
-				case 10:
-					month = "октября";
-					break;
-				case 11:
-					month = "ноября";
-					break;
-				case 12:
-					month = "декабря";
-			}
-			return d.getDate() + ' ' + month + ' ' + d.getFullYear();
+		var day = d.getDate();
+		var month = d.getMonth();
+		var year = d.getFullYear();
+		var ru = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+			'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+		
+		// Month: Verbal output
+		if (param === 'ru') {
+			month = ru[month];
+			return day + ' ' + month + ' ' + year;
 		}
+		
+		// Month: Numeric output
+		month = month + 1;
 		if (month < 10) {
 			month = '0' + month;
 		}
-		return d.getDate() + '.' + month + '.' + d.getFullYear();
+		if (param === 'r') {
+			return year + '.' + month + '.' + day;
+		}
+		return day + '.' + month + '.' + year;
 	}
 };
