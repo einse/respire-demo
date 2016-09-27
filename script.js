8d704f496f94a7fd028b9dd36a59c2c0ab38dd0a
Serguey Arsentev <einse@yandex.ru>
2016-09-27 08:53:50 +0000

    Add Ctrl+Enter handler for creating new entries

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
+var keyName = {
+	Ctrl: 17,
+	Enter: 13
+};
+
+var keyMap = {
+	17: false,
+	13: false
+};
+
+$(document).keydown(function (event) {
+	var $focused;
+	
+	if (event.keyCode in keyMap) {
+		keyMap[event.keyCode] = true;
+	}
+	
+	if (keyMap[keyName['Ctrl']] && keyMap[keyName['Enter']]) {
+		// On Ctrl+Enter, create new entry
+		$focused = $(".entry:focus");
+		$focused.after('<div class="entry" contentEditable="true"></div>').next().focus();
+	}
+});
+
+$(document).keyup(function (event) {
+	if (event.keyCode in keyMap) {
+		keyMap[event.keyCode] = false;
+	}
+});
+
 $(".entry:first").html(Calendar.today('ru'));
 $(".entry:last").focus();
