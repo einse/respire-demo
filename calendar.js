45176b9f814e7053b01e437c7f42e6d8feb9222c
Serguey Arsentev <einse@yandex.ru>
2016-10-03 14:20:19 +0000

    Add isValid() function implementation

--- calendar.js
+++ calendar.js
@@ -1,11 +1,17 @@
 /**
  * Calendar is a part of Respire:
  * https://github.com/einse/respire
- * 
- * The date which is using for examples is October 1, 2016
  */
 
 var Calendar = {
+	/**
+	 * today(param);
+	 * 
+	 * Returns the present day in various formats.
+	 * 
+	 * The date used in all examples for this function
+	 * is October 1, 2016
+	 */
 	today: function (param) {
 		var d = new Date();
 		var day = d.getDate();
@@ -51,7 +57,9 @@ var Calendar = {
 		}
 		
 		// Output example: 2016-10-01
-		if (param === 'r-') {
+		// This format is ISO standard. Learn more: 
+		// http://www.w3schools.com/js/js_date_formats.asp
+		if (param === 'r-' || param === 'iso') {
 			return year + '-' + month + '-' + day;
 		}
 		
@@ -69,7 +77,90 @@ var Calendar = {
 		// 01.10.2016
 		return day + '.' + month + '.' + year;
 	},
+	/**
+	 * isValid(dateString);
+	 * 
+	 * Returns true or false whether dateString represents 
+	 * a correct date (using Proleptic Gregorian calendar). 
+	 * 
+	 * The parameter dateString should contain
+	 * an ISO-formatted date. For example: "2016-01-03" for 
+	 * October 3, 2016.
+	 * 
+	 * Dates under 1000-01-01 are not supported: therefore
+	 * Calendar.isValid('999-12-31') {or earlier} will return false;
+	 */
 	isValid: function (dateString) {
+		var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		var monthNumberJS;
+		var isLeapYear = false;
+		
+		/** 
+		 * Parsing the dateString
+		 * (Only positive numbers will pass.)
+		 */
+		var parsed = dateString.split('-');
+		if (parsed.length !== 3) {
+			//~ console.log('array length');
+			return false;
+		}
+		for (var value in parsed) {
+			parsed[value] = Number(parsed[value]);
+			if (parsed[value] === 0) {
+				//~ console.log('zero');
+				return false;
+			}
+			if (isNaN(parsed[value])) {
+				//~ console.log('type', typeof parsed[value], parsed[value]);
+				return false;
+			}
+		}
+		
+		/**
+		 * Year number test
+		 */
+		if (parsed[0] < 1000) {
+			console.error("Calendar.js: Dates under 1000 CE are not supported.");
+			return false;
+		}
+		
+		/** 
+		 * Leap year test
+		 */
+		isLeapYear = false;
+		if (parsed[0] % 400 === 0)
+		{
+			isLeapYear = true;
+		} else {
+			if (parsed[0] % 4 === 0) {
+				isLeapYear = true;
+			}
+			if (parsed[0] % 100 === 0) {
+				isLeapYear = false;
+			}
+		}
+		
+		/** 
+		 * Month number test
+		 */
+		if (parsed[1] > 12) {
+			//~ console.log('month > 12');
+			return false;
+		}
+		
+		/**
+		 * Day number test
+		 */
+		monthNumberJS = parsed[1] - 1;
+		if (parsed[2] > dayCount[monthNumberJS]) {
+			if (monthNumberJS === 1 && parsed[2] === 29 && isLeapYear) {
+				return true;
+			}
+			//~ console.log('day test failed');
+			return false;
+		}
+		
+		return true;
 	},
 	prev: function (dateString) {
 	},
