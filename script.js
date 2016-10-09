0c5194528edabff37b6b16328b516e26f4d58c8d
Serguey Arsentev <einse@yandex.ru>
2016-10-09 10:56:22 +0300

    Add 'data-a'; add new todo; test buttons

--- script.js
+++ script.js
@@ -54,11 +54,22 @@ $(document).keyup(function (event) {
 
 // TODO: See .data() in jQuery 3.x
 // TODO: Fix for multiple set
-$(".set:first").attr('data-d', C$.prev(C$.today()));
-$(".set:first .set__entry:first").html(C$.prev(C$.today()));
-$(".set:nth-child(2)").attr('data-d', C$.today());
-$(".set:nth-child(2) .set__entry:first").html(C$.today());
-$(".set:last").attr('data-d', C$.next(C$.today()));
-$(".set:last .set__entry:first").html(C$.next(C$.today()));
+// TODO: 
+// 1. Create one column.
+// 2. Set 'data-d'.
+// 3. Write 'data-d' to 'set__entry:first'.
+// 4. Add 2 additional columns for yesterday and tomorrow.
+// 5. Call prev('today') & next('today')
+//    & then repeat (2-3) for added columns.
+//~ $(".set:first").attr('data-d', C$.prev(C$.today()));
+//~ $(".set:first .set__entry:first").html(C$.prev(C$.today()));
+//~ $(".set:nth-child(2)").attr('data-d', C$.today());
+//~ $(".set:nth-child(2) .set__entry:first").html(C$.today());
+//~ $(".set:last").attr('data-d', C$.next(C$.today()));
+//~ $(".set:last .set__entry:first").html(C$.next(C$.today()));
 
-$(".set:nth-child(2) .set__entry:last").focus();
+//~ $(".set:nth-child(2) .set__entry:last").focus();
+
+
+// TODO: Add button press handling
+$('.mount__button').click(function(event) {console.log(event.target);});
