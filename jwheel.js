c76cc566e82a468fb7adc4edde896d184d521dbf
Serguey Arsentev <einse@yandex.ru>
2016-10-09 16:38:16 +0300

    Fix mouse scrolling

--- jwheel.js
+++ jwheel.js
@@ -1,7 +1,7 @@
 /* From http://bizikov.ru/blog/gorizontalnyj-skroll-sajta/ */
 
 $(function () {
-	$("#paper").mousewheel(function (event, delta) {
+	$('body').mousewheel(function (event, delta) {
 		this.scrollLeft -= (delta * 100);
 		event.preventDefault();
 	});
