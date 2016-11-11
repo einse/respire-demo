581e73c3bf2e4db8fd98781018a49d0325573f83
Serguey Arsentev <einse@yandex.ru>
2016-11-11 16:57:58 +0000

    Rewrite functions which load new sets

--- script.js
+++ script.js
@@ -1,120 +1,105 @@
-window.onload = function () {
-	function addBefore(dateToSet) {
-		// TODO: Find any other solutions
-		var content = '<div class="set" data-d="' + dateToSet + '">\
-				<div class="set__entry">' + dateToSet + '</div>\
-				<div class="set__entry" contenteditable></div>\
-			</div>'
-		$('.set:first').before(content);
-	};
-	
-	function addAfter(dateToSet) {
-		// TODO: Find any other solutions
-		var content = '<div class="set" data-d="' + dateToSet + '">\
-				<div class="set__entry">' + dateToSet + '</div>\
-				<div class="set__entry" contenteditable></div>\
-			</div>'
-		$('.set:last').after(content);
-	};
-	
-	var keyName = {
-		Ctrl: 17,
-		Enter: 13
-	};
+(function () {
+    function loadBefore(dateToSet) {
+        var set_ = document.createElement('div');
+        set_.className = 'set';
+        set_.setAttribute('data-d', dateToSet);
+        
+        var title_entry = document.createElement('div');
+        title_entry.className = 'set__entry';
+        title_entry.innerText = dateToSet;
+        
+        var ordinary_entry = document.createElement('div');
+        ordinary_entry.className = 'set__entry';
+        ordinary_entry.setAttribute('contentEditable', '');
+        
+        set_.appendChild(title_entry);
+        set_.appendChild(ordinary_entry);
+        
+        var content = set_;
+        $('.set:first').before(content);
+    };
+    
+    function loadAfter(dateToSet) {
+        var set_ = document.createElement('div');
+        set_.className = 'set';
+        set_.setAttribute('data-d', dateToSet);
+        
+        var title_entry = document.createElement('div');
+        title_entry.className = 'set__entry';
+        title_entry.innerText = dateToSet;
+        
+        var ordinary_entry = document.createElement('div');
+        ordinary_entry.className = 'set__entry';
+        ordinary_entry.setAttribute('contentEditable', '');
+        
+        set_.appendChild(title_entry);
+        set_.appendChild(ordinary_entry);
+        
+        var content = set_;
+        $('.set:last').after(content);
+    };
+    
+    var keyName = {
+        Ctrl: 17,
+        Enter: 13
+    };
 
-	var keyMap = {
-		17: false,
-		13: false
-	};
+    var keyMap = {
+        17: false,
+        13: false
+    };
 
-	$(document).keydown(function (event) {
-		var $focused;
-		var offsetTop;
-		var $prev;
-		var prev;
-		var prevOffsetTop;
-		var prevParentNodeWidth;
-		
-		if (event.keyCode in keyMap) {
-			keyMap[event.keyCode] = true;
-		}
-		
-		if (keyMap[keyName['Ctrl']] && keyMap[keyName['Enter']]) {
-			// On Ctrl+Enter, create new entry
-			$focused = $(".set__entry:focus");
-			$focused.after('<div class="set__entry" contenteditable></div>').next().focus();
-			$focused = $(".set__entry:focus");
-			offsetTop = $focused[0].offsetTop;
-			//~ console.log(offsetTop);
-			//~ $focused.attr('data-offsetTop', offsetTop);
-			
-			$prev = $focused.prev();
-			prev = $prev[0];
-			prevOffsetTop = prev.offsetTop;
-			if (offsetTop < prevOffsetTop) {
-				//~ console.log('less! clientWidth', prev.clientWidth);
-				//~ console.log('less! offsetWidth', prev.offsetWidth);
-				//~ console.log('less! parentNode.offsetWidth', prev.parentNode.offsetWidth);
-				//~ console.log('less! parentNode.clientWidth', prev.parentNode.clientWidth);
-				prevParentNodeWidth = $(prev.parentNode).width();
-				//~ console.log(prevParentNodeWidth);
-				$(prev.parentNode).width(prevParentNodeWidth + prev.clientWidth);
-				prevParentNodeWidth = $(prev.parentNode).width();
-				//~ console.log(prevParentNodeWidth);
-			}
-		}
-	});
+    $(document).keydown(function (event) {
+        var $focused;
+        var offsetTop;
+        var $prev;
+        var prev;
+        var prevOffsetTop;
+        var prevParentNodeWidth;
+        
+        if (event.keyCode in keyMap) {
+            keyMap[event.keyCode] = true;
+        }
+        
+        if (keyMap[keyName['Ctrl']] && keyMap[keyName['Enter']]) {
+            // On Ctrl+Enter, create new entry
+            $focused = $(".set__entry:focus");
+            $focused.after('<div class="set__entry" contenteditable></div>').next().focus();
+            $focused = $(".set__entry:focus");
+        }
+    });
 
-	$(document).keyup(function (event) {
-		if (event.keyCode in keyMap) {
-			keyMap[event.keyCode] = false;
-		}
-	});
+    $(document).keyup(function (event) {
+        if (event.keyCode in keyMap) {
+            keyMap[event.keyCode] = false;
+        }
+    });
 
-	// TODO: See .data() in jQuery 3.x
-	// TODO: Fix for multiple set
-	// TODO: 
-	// 1. Create one column.
-	// 2. Set 'data-d'.
-	// 3. Write 'data-d' to 'set__entry:first'.
-	// 4. Add 2 additional columns for yesterday and tomorrow.
-	// 5. Call prev('today') & next('today')
-	//    & then repeat (2-3) for added columns.
-	//~ $(".set:first").attr('data-d', C$.prev(C$.today()));
-	//~ $(".set:first .set__entry:first").html(C$.prev(C$.today()));
-	//~ $(".set:nth-child(2)").attr('data-d', C$.today());
-	//~ $(".set:nth-child(2) .set__entry:first").html(C$.today());
-	//~ $(".set:last").attr('data-d', C$.next(C$.today()));
-	//~ $(".set:last .set__entry:first").html(C$.next(C$.today()));
-
-	//~ $(".set:nth-child(2) .set__entry:last").focus();
-
-	$('.set:first').attr('data-d', C$.today());
-	$(".set:first .set__entry:first").html($('.set:first').attr('data-d'));
-	$(".set:first .set__entry:last").focus();
-
-	// TODO: Add button press handling
-	$('.mount__button').click(function(event) {
-		var earliest = {};
-		var latest = {};
-		var action;
-		
-		earliest.date = $('.set:first').attr('data-d');
-		latest.date = $('.set:last').attr('data-d');
-		
-		action = $(event.target).attr('data-a');
-		
-		if (action === 'b') {
-			for (var i = 0; i < 3; i++) {
-				addBefore(C$.prev(earliest.date));
-				earliest.date = $('.set:first').attr('data-d');
-			}
-		}
-		if (action === 'f') {
-			for (var i = 0; i < 3; i++) {
-				addAfter(C$.next(latest.date));
-				latest.date = $('.set:last').attr('data-d');
-			}
-		}
-	});
-};
+    $('.mount__button').click(function(event) {
+        var earliest = {};
+        var latest = {};
+        var action;
+        
+        earliest.date = $('.set:first').attr('data-d');
+        latest.date = $('.set:last').attr('data-d');
+        
+        action = $(event.target).attr('data-a');
+        
+        if (action === 'b') {
+            for (var i = 0; i < 3; i++) {
+                loadBefore(C$.prev(earliest.date));
+                earliest.date = $('.set:first').attr('data-d');
+            }
+        }
+        if (action === 'f') {
+            for (var i = 0; i < 3; i++) {
+                loadAfter(C$.next(latest.date));
+                latest.date = $('.set:last').attr('data-d');
+            }
+        }
+    });
+    
+    $('.set:first').attr('data-d', C$.today());
+    $(".set:first .set__entry:first").html($('.set:first').attr('data-d'));
+    $(".set:first .set__entry:last").focus();
+})();
